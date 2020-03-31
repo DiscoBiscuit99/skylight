5c088f46fee06889ee18a20f67868018e5a51f18
DiscoBiscuit99 <creme3sandwich@gmail.com>
2020-03-31 12:21:34 +0200

    implemented constant overlay, disabled picture-in-picture mode, window now spawns in lower right corner of current display

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-const { app, globalShortcut, BrowserWindow } = require('electron');
+const { app, screen, globalShortcut, BrowserWindow } = require('electron');
 
 let win;
 
 function createWindow() {
 	win = new BrowserWindow({
-		width: 1000,
-		height: 800,
+		width: 50 * 16,
+		height: 50 * 9,
 		frame: false,
+		show: false,
 		webPreferences: {
 			nodeIntegration: true,
 		},
 	});
 
-	win.loadFile('index.html');
+	let display = screen.getDisplayNearestPoint(screen.getCursorScreenPoint());
+
+	let pos = [ display.size.width - win.getSize()[0], display.size.height - win.getSize()[1] ];
+
+	win.setPosition(pos[0], pos[1]);
+
+	win.loadFile('src/index.html');
+
+	win.setAlwaysOnTop(true);
+
+	win.once('ready-to-show', () => {
+		win.show();
+	});
 }
 
 app.whenReady().then(createWindow);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const { remote } = require('electron');
-const dialog = remote.dialog;
 
-let videoSrc = remote.process.argv[2];
+const { process, dialog } = remote;
 
-console.log(videoSrc);
+let videoSrc = process.argv[2];
 
 let videoframe = document.getElementById("video-frame");
 
@@ -16,6 +15,7 @@ if (videoSrc) {
 	video.controls = true;
 	video.autofocus = true;
 	video.autoplay = true;
+	video.disablePictureInPicture = true;
 
 	let source = document.createElement("source");
 
@@ -25,6 +25,10 @@ if (videoSrc) {
 
 	video.appendChild(source);
 	videoframe.appendChild(video);
+
+	if (!video.focus) {
+		video.focus = true;
+	}
 }
 
 let draggableRegion = document.createElement("div");
@@ -33,9 +37,11 @@ draggableRegion.id = "draggable-region";
 
 videoframe.appendChild(draggableRegion);
 
-if (!video.focus) {
-	video.focus = true;
-}
+//let undraggableRegion = document.createElement("div");
+
+//undraggableRegion.id = "undraggable-region";
+
+//videoframe.appendChild(undraggableRegion);
 
 // Keybindings //
 
@@ -54,8 +60,6 @@ Mousetrap.bind('f', () => {
 		video.webkitRequestFullscreen();
 	}
 });
-
-/// Time regarding keybindings.
 
 // Go 5 seconds backward.
 Mousetrap.bind('left', () => {
@@ -77,3 +81,47 @@ Mousetrap.bind('.', () => {
 	let currentTime = video.currentTime;
 	video.currentTime = currentTime + 2;
 });
+
+// Set current playback time by percentage.
+
+// Set to 10%.
+Mousetrap.bind('1', () => {
+	video.currentTime = (video.duration / 100) * 10;  // 10%.
+});
+// Set to 20%.
+Mousetrap.bind('2', () => {
+	video.currentTime = (video.duration / 100) * 20;  // 20%.
+});
+// Set to 30%.
+Mousetrap.bind('3', () => {
+	video.currentTime = (video.duration / 100) * 30;  // 30%.
+});
+// Set to 40%.
+Mousetrap.bind('4', () => {
+	video.currentTime = (video.duration / 100) * 40;  // 40%.
+});
+// Set to 50%.
+Mousetrap.bind('5', () => {
+	video.currentTime = (video.duration / 100) * 50;  // 50%.
+});
+// Set to 60%.
+Mousetrap.bind('6', () => {
+	video.currentTime = (video.duration / 100) * 60;  // 60%.
+});
+// Set to 70%.
+Mousetrap.bind('7', () => {
+	video.currentTime = (video.duration / 100) * 70;  // 70%.
+});
+// Set to 80%.
+Mousetrap.bind('8', () => {
+	video.currentTime = (video.duration / 100) * 80;  // 80%.
+});
+// Set to 90%.
+Mousetrap.bind('9', () => {
+	video.currentTime = (video.duration / 100) * 90;  // 90%.
+});
+// Set to 100%.
+Mousetrap.bind('0', () => {
+	video.currentTime = video.duration;  // 100%.
+});
+
